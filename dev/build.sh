3d4c3088e6878ac38ec2f84274ce9f93dbac9f37
lucyrpedro <lu.ufmg@gmail.com>
2019-07-17 18:27:51 +0100

    New build.sh for Python

--- dev/build.sh
+++ dev/build.sh
@@ -11,6 +11,11 @@ git clone https://github.com/Unidata/netcdf4-python.git
 
 cd netcdf4-python
 
+patch -p1 --dry-run < ../v1.patch
+
+patch -p1 < ../v1.patch
+
+
 # prerequisites to build netcdf4-python:
 # * Make sure [numpy](http://www.numpy.org/) and [Cython](http://cython.org/) are
 #  installed and you have [Python](https://www.python.org) 2.7 or newer.
@@ -19,13 +24,17 @@ cd netcdf4-python
 # * Run `python3 setup.py build`, then `python setup.py install` (with `sudo` if necessary).
 # MPI4PY is necessary!
 # pip3 install mpi4py
-python3 setup.py build
 
-echo "Check that the right NetCDF library is used"
-ldd build/lib.linux-x86_64-3.6/netCDF4/_netCDF4.cpython-36m-x86_64-linux-gnu.so | grep netcdf
+# for Ubuntu:
+# apt-get install cython3 python3-numpy python3-mpi4py
+# Make sure you purged the hdf5 serial version:
+# sudo apt-get purge libhdf5-serial-dev libhdf5-dev
 
 python3 setup.py install --user
 # * To run all the tests, execute `cd test && python run_all.py`.
+
+echo "Check that the right NetCDF library is used"
+ldd build/lib.linux-x86_64-3.6/netCDF4/_netCDF4.cpython-36m-x86_64-linux-gnu.so | grep netcdf
 
 echo "Setup ESDM as target for tempfiles"
 cd test
