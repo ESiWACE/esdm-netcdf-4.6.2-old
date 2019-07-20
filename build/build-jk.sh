c6d7f795d770c23ddfdbdb495602cc5f6c8ff605
Julian M. Kunkel <juliankunkel@googlemail.com>
2019-07-20 15:53:33 +0100

    Updated test.

--- build/build-jk.sh
+++ build/build-jk.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+cd ..
+autoreconf
+cd build
 ../configure --prefix=/home/kunkel/ur-git/esiwace/ESD-Middleware/install/ --with-esdm="/home/kunkel/ur-git/esiwace/ESD-Middleware/install" CFLAGS="-I/usr/include/hdf5/openmpi/ -g3" LDFLAGS="-L/usr/lib/x86_64-linux-gnu/hdf5/openmpi/" CC=mpicc  --disable-dap
