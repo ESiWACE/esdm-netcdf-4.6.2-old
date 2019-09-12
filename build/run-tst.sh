4a04f1e0c8c191283195265722aba4965a9e7712
lucyrpedro <lu.ufmg@gmail.com>
2019-09-12 15:34:04 +0100

    Script to run the C tests.

---
+++ build/run-tst.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+pushd libsrcesdm_test/
+
+for I in $(ls ../../libsrcesdm_test/ | grep "\.c$") ; do
+  make ${I%%.c}
+done
+
+export NC_ESDM_FORCEESDM=1
+export ESDM_LOGLEVEL_BUFFER=10
+
+for I in $(ls ../../libsrcesdm_test/ | grep "\.c$") ; do
+  I=${I%%.c}
+  if [[ -e $I ]] ; then
+    echo "Running $I"
+    ./$I
+    if [[ $? != 0 ]] ; then
+      break
+    fi
+    echo "OK"
+    sleep 1
+  fi
+done
+
+popd
