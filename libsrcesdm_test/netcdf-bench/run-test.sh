2422afac914c114d191399384fd6de986b2cba56
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-04-08 20:32:40 +0100

    Added mkfs

--- libsrcesdm_test/netcdf-bench/run-test.sh
+++ libsrcesdm_test/netcdf-bench/run-test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mkfs.esdm -g -l --create --remove
 ./benchtool -f=esdm://longtest -w -r
 
+mkfs.esdm -g -l --create --remove
 mpiexec -np 2 ./src/benchtool -f="esdm://test.esdm" -n=1 -p=2
 
 
