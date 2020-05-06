57b4bd2cb1f2d267abac7e9c46a8ffd3378897aa
lucyrpedro <lu.ufmg@gmail.com>
2020-05-06 08:22:11 +0100

    Adding build.sh

---
+++ build/build.sh
@@ -0,0 +1,1 @@
+../configure --prefix=/home/user/esiwace/esdm/install/ --with-esdm="/home/user/esiwace/esdm/install" CFLAGS="-I/usr/include/hdf5/openmpi/ -g3" LDFLAGS="-L/usr/lib/x86_64-linux-gnu/hdf5/openmpi/" CC=mpicc  --disable-dap --enable-parallel-tests
